94ddfe1fcbd1fbcef4195f37c0886d1fc2f5a38d
JonAbood <JonAbood@users.noreply.github.com>
2016-05-12 12:01:07 -0400

    Add files via upload

---
+++ StonesGame/StonesGameOG/StonesGame_a.java
@@ -0,0 +1,162 @@
+
+/**
+ * Write a description of class Stone here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+import javax.swing.JOptionPane;
+public class StonesGame_a
+{
+   
+	public static void main(String[]args)
+	{
+    	//craete vars
+    	int stones, number=0;
+    	String answer;
+    	//generate a random number between 15 and 30
+    	stones=15+(int)(Math.random()*(30-15+1));
+    	//while that number is greater than 0
+    	while (stones>0)
+    	{
+        	number=0;
+        	while (number<1||number>3||number>stones)
+        	{
+            	//display message box with the current number of stones and asks how many you'd like.
+            	answer=JOptionPane.showInputDialog("There are "+stones+" stones. \nHow many would you like?");
+            	number=Integer.parseInt(answer);
+            	if (number<1||number>3||number>stones)
+            	{
+                	answer="Invalid number of stones!!!";
+                	JOptionPane.showMessageDialog(null,answer);
+            	}
+        	}
+        	//subtract that number from the current total
+        	stones=stones-number;
+        	//if the number of stones is less than 1
+        	if (stones==0)
+        	{
+            	//display answer box to say that player 1 loses
+            	answer="COMPUTER WINS!!";
+            	JOptionPane.showMessageDialog(null,answer);
+        	}
+        	//else
+        	else
+        	{
+            	number=0;
+            	while (number<1||number>3||number>stones)
+            	{
+                	if (stones==1)
+                	{
+                    	number=1;
+                	}
+                	else if (stones==2)
+                	{
+                    	number=1;
+                	}
+                	else if (stones==3)
+                	{
+                    	number=2;
+                	}
+                	else if (stones==4)
+                	{
+                    	number=3;
+                	}
+                	else if (stones==6)
+                	{
+                    	number=1;
+                	}
+                	else if (stones==7)
+                	{
+                    	number=2;
+                	}
+                	else if (stones==8)
+                	{
+                    	number=3;
+                	}
+                	else if (stones==10)
+                	{
+                    	number=1;
+                	}
+                	else if (stones==11)
+                	{
+                    	number=2;
+                	}
+                	else if (stones==12)
+                	{
+                    	number=3;
+                	}
+                	else if (stones==14)
+                	{
+                    	number=1;
+                	}
+                	else if (stones==15)
+                	{
+                    	number=2;
+                	}
+                	else if (stones==16)
+                	{
+                    	number=3;
+                	}
+                	else if (stones==18)
+                	{
+                    	number=1;
+                	}
+                	else if (stones==19)
+                	{
+                    	number=2;
+                	}
+                	else if (stones==20)
+                	{
+                    	number=3;
+                	}
+                	else if (stones==22)
+                	{
+                    	number=1;
+                	}
+                	else if (stones==23)
+                	{
+                    	number=2;
+                	}
+                	else if (stones==24)
+                	{
+                    	number=3;
+                	}
+                	else if (stones==26)
+                	{
+                    	number=1;
+                	}
+                	else if (stones==27)
+                	{
+                    	number=2;
+                	}
+                	else if (stones==28)
+                	{
+                    	number=3;
+                	}
+                	else if (stones==30)
+                	{
+                    	number=1;
+                	}
+                	else
+                	{
+                    	number=1+(int)(Math.random()*(3-1+1));
+                	}
+                	answer="There are "+stones+" stones \nThe computer takes "+number;
+                	JOptionPane.showMessageDialog(null,answer);
+            	}
+            	//subtract that number from the current total
+            	stones=stones-number;
+            	//if the number of stones is less than 1
+            	if (stones==0)
+            	{
+                	//display answer box to say that player 1 loses
+                	answer="YOU WIN!!";
+                	JOptionPane.showMessageDialog(null,answer);
+            	}
+        	}
+    	}
+	}
+}
+	
+
